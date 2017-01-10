b1e659d34582f3723905f59a6992f3ddcb1d029f
GilTads <gil.tads@gmail.com>
2017-01-10 00:26:44 -0300

    criando icon e splaschs-screen

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,15 @@
-$('.collection-item').on('click', function(){
+$('.collection').on('click', '.collection-item', function(){
      
    var $badge = $('.badge', this);
     
-    if($badge.length == 0){
+    if($badge.length === 0){
         $badge = $('<span class="badge brown-text">0</span>').appendTo(this);
     }
     
     $badge.text(parseInt($badge.text()) + 1);
+    
+    var nomeProduto = this.firstChild.textContent;
+    Materialize.toast(nomeProduto + ' adicionado', 2000);
 });
 
 //Inicializa o plugin de modal do materialize
@@ -17,13 +20,35 @@ $('.modal-trigger').leanModal();
 
 
 $('#confirmar').on('click', function(){
-    var texto = ' ';
+    var texto = '';
     $('.badge').parent().each(function(){
-        var produto = this.firstChild.textContent;
-        var quantidade = this.lastChild.textContent;
-        texto += (produto+ ': '+ quantidade+ ', ');
-        
+        texto += this.firstChild.textContent + ': ';
+        texto += this.lastChild.textContent + ', ';
     });
-    $('#resumo').text(texto);
+    
+    $('#resumo').empty().text(texto)
+});
+    
+    
+
+
+$('.collection').on('click', '.badge', function(){
+    $(this).remove();
+    return false;
+});
+
+$('.limpar').on('click', function(){
+    $('#numero-mesa').val('');
+    $('.badge').remove();
+});
+
+$('.dropdown-button').dropdown({
+  inDuration: 300,
+  outDuration: 225,
+  constrain_width: false, // Does not change width of dropdown to that of the activator
+  hover: true, // Activate on hover
+  gutter: 5, // Spacing from edge
+  belowOrigin: false, // Displays dropdown below the button
+  alignment: 'left' // Displays dropdown with edge aligned to the left of button
 });
 
